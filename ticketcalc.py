5246200c8dbb3c799f221294968bbd6f64fed390
tqbed <66844373+tqbed@users.noreply.github.com>
2024-06-21 21:34:08 -0400

    I am dumb and W3Schools told me how a dictionary works

--- ticketcalc.py
+++ ticketcalc.py
@@ -1,30 +1,30 @@
-#all values in CAD per ticket
+#all values in CAD 
 items = {
-        "OpenAI Credits": 3.425,
-        "Domain": 3.425,
-        "Notebook": 3.288,
-        "Logic Analyzer": 3.4,
-        "Breadboard": 2.5,
-        "Multimeter": 2.14,
-        "Arcade Ticket Counter": 2.14,
-        "Soldering Iron": 3.75,
-        "Pinecil": 3,
-        "Yubikey": 5.022,
-        "Keycaps": 1.826,
-        "Octocat": 1.933,
-        "Wacom": 3.2,
-        "Backpack": 3.67,
-        "Flipper": 3.32,
-        "Keyboard": 3.1,
-        "Framework Factory Seconds": 5.66,
-        "Prusa": 4.51,
-        "Bambu Lab": 3.33,
-        "Framework 13 Inch": 10.9,
-        "Quest 3": 3.25,
-        "Framework 16 Inch": 5.75,
-        "Macbook": 3.8725
+        "OpenAI Credits": [4, 13.7],
+        "Domain": [4, 13.7],
+        "Notebook": [5, 16.44],
+        "Logic Analyzer": [5, 17],
+        "Breadboard": [6, 15],
+        "Multimeter": [7, 15],
+        "Arcade Ticket Counter": [7, 15],
+        "Soldering Iron": [8, 30],
+        "Pinecil": [14, 42],
+        "Yubikey": [15, 75.33],
+        "Keycaps": [15, 27.39],
+        "Octocat": [15, 29],
+        "Wacom": [25, 80],
+        "Backpack": [50, 183.53],
+        "Flipper": [70, 232.85],
+        "Keyboard": [75, 232.85],
+        "Framework Factory Seconds": [120, 679],
+        "Prusa": [130, 587],
+        "Bambu Lab": [135, 450],
+        "Framework 13 Inch": [175, 1909],
+        "Quest 3": [200, 650],
+        "Framework 16 Inch": [400, 2299],
+        "Macbook": [400, 1549]
     }
-
+  
 print("Press 1 to find the price per ticket for an item.")
 #print("Press 2 to find the most valuable items for a given number of tickets.")
 choice = int(input("Enter your choice: "))
@@ -32,7 +32,7 @@ choice = int(input("Enter your choice: "))
 if choice == 1:
     item_name = input("Enter the name of the item: ")
     if item_name in items:
-        price_per_ticket = items[item_name]
-        print("the value of a " + item_name + " is " + str(price_per_ticket) "CAD per ticket")
+        price_per_ticket = items[item_name][1] / items[item_name][0]
+        print("the value of a " + item_name + " is " + str(price_per_ticket) + " CAD per ticket")
     else:
         print("item not found, check for typos and try again")
